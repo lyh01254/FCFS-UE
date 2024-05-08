df46f666c210e6d7df2bc329e3a4f438162a1ab0
lyh01254 <yuhao.liu@nyu.edu>
2024-05-07 20:41:38 -0400

    5.7

--- source/curve.cpp
+++ source/curve.cpp
@@ -89,102 +89,129 @@ void find_v(std::vector<Curve>& v, const std::vector<double>& demand, const std:
 }
 
 void raise(Curve& raised_V, const Curve& V, const Curve& v, const double& gamma){
+    //the first end of the raised curve
     double raised_x = v.x[v.NbSegs] - v.x[0];
     double raised_y = v.y[v.NbSegs] + V.y[0];
     double raised_slope = 0;
     raised_V.x.push_back(raised_x);
     raised_V.y.push_back(raised_y);
-    double Dt = v.x[0];
+    raised_V.con_index.push_back(0);
+
+    double Dt = v.x[0]; //the threshold Dt
     int idx = 0;
-    raised_V.con_index.push_back(idx);
-    while (idx < V.NbEnds){
+    while (idx < V.NbEnds - 1){ //
          if (V.x[idx] < Dt && V.x[idx+1] > Dt){
-            //todo: seglen_x and seglen_y
             //*First handle the raised segment
             double new_slope = V.slopes[idx] + gamma;
-            double new_len = Dt - V.x[idx];
-            raised_x += new_len; //the next x
-            raised_y += new_len * new_slope; //the next y
+            double len_x = Dt - V.x[idx];
+            double len_y = len_x * new_slope;
+            raised_x += len_x; //the next x
+            raised_y += len_y; //the next y
             if (new_slope > raised_slope && idx > 0) { //if convexity changes
                 raised_V.con_index.push_back(idx); //current end is a split
             }
             raised_V.x.push_back(raised_x);
             raised_V.y.push_back(raised_y);
             raised_V.slopes.push_back(new_slope);
-            raised_V.SegLen_x.push_back(new_len);
+            raised_V.SegLen_x.push_back(len_x);
+            raised_V.SegLen_y.push_back(len_y);
 
             //*Then handle the remaining segment
             new_slope = V.slopes[idx];
-            new_len = V.x[idx+1] - Dt;
-            raised_x += new_len; //the next x
-            raised_y += new_len * new_slope; //the next y
+            len_x = V.x[idx+1] - Dt;
+            len_y = len_x * new_slope;
+            raised_x += len_x; //the next x
+            raised_y += len_y; //the next y
             //*Note: convexity must maintain
             raised_slope = new_slope; //the next slope //*Note: new_slope must != old_slope
             raised_V.x.push_back(raised_x);
             raised_V.y.push_back(raised_y);
             raised_V.slopes.push_back(new_slope);
-            raised_V.SegLen_x.push_back(new_len);
+            raised_V.SegLen_x.push_back(len_x);
+            raised_V.SegLen_y.push_back(len_y);
             ++idx;
         } else { 
-            //todo: a special case where old_slope == new slope
             double new_slope = V.slopes[idx];
             if (V.x[idx+1] <= Dt){
                 new_slope += gamma;
             } 
-            raised_x += V.SegLen_x[idx]; //the next x
-            raised_y += V.SegLen_x[idx] * new_slope; //the next y
+            double len_x = V.SegLen_x[idx];
+            double len_y = len_x * new_slope;
+            raised_x += len_x; //the next x
+            raised_y += len_y; //the next y
             if (new_slope > raised_slope && idx > 0){ //if convexity changes
                 raised_V.con_index.push_back(idx); //current end is a split
             } 
-            raised_slope = new_slope; //the next slope //*Note: new_slope must != old_slope
-            raised_V.x.push_back(raised_x);
-            raised_V.y.push_back(raised_y);
-            raised_V.slopes.push_back(raised_slope);
-            raised_V.SegLen_x.push_back(V.SegLen_x[idx]);
+            if (new_slope != raised_slope){ //there is a special case where new_slope == old_slope
+                raised_slope = new_slope; //the next slope 
+                raised_V.x.push_back(raised_x);
+                raised_V.y.push_back(raised_y);
+                raised_V.slopes.push_back(raised_slope);
+                raised_V.SegLen_x.push_back(len_x);
+                raised_V.SegLen_y.push_back(len_y);
+            } else {
+                raised_V.x.back() = raised_x;
+                raised_V.y.back() = raised_y;
+                raised_V.SegLen_x.back() += len_x;
+                raised_V.SegLen_y.back() += len_y;
+            }
             ++idx;         
         }
     }
+    raised_V.NbEnds = raised_V.x.size();
+    raised_V.NbSegs = raised_V.NbEnds - 1;
+    raised_V.NbConsegs = raised_V.con_index.size() - 1;
 };
 
 void inf_convolute(std::vector<Curve>& IC_curves, const Curve& V, const Curve& v){
     int End_v = v.x.back(); //last end index of v
     for (int i = 0; i < V.NbConsegs; i++){ //for each concave segment
-        int idx_v = 0; //index of ends for v
-        int idx_V = V.con_index[i]; //index of ends for V's concave segment i
-        int End_V = V.con_index[i+1];
-        double x_V = V.x[idx_V]; //starting x of concave segment 
-        double x_v = v.x[0]; //starting x of v; i.e., Dt
-        double x = x_V; //starting x of IC_curve
+        int idx_v = 0; //iterative index of ends for v
+        int idx_V = V.con_index[i]; //iterative index of ends for V's concave segment i
+        int End_V = V.con_index[i+1]; //final end index of V's concave segment i
+        double x = V.x[idx_V]; //starting x of IC_curve
         double y = v.y[0]+V.y[i]; //starting y of IC_curve
         IC_curves[i].x.push_back(x); //push back the starting x
         IC_curves[i].y.push_back(y); //push back the starting y
         //*slope re-arrange
+        double len_x, len_y, slope_to_add;
         while (idx_v < End_v || idx_V < End_V){ //there's still slope to insert
-            if (idx_v == End_v || V.slopes[idx_V] >= v.slopes[idx_v]){ //condition for inserting the next slope in V
-                //todo: a new segment
-                double slope_to_add = v.slopes[idx_v];
-                x += v.SegLen[idx_v];
-                y += 
-            } else { //otherwise, insert the next slope in v
-                if (IC_curves[i].slopes.back() != v.slopes[idx_v]){
-                    //todo: extension of the previous
-                } else {
-                    //todo: a new segment
-                }
+            if (idx_v == End_v || V.slopes[idx_V] >= v.slopes[idx_v]){ //insert next slope from V
+                len_x = V.SegLen_x[idx_V];
+                len_y = V.SegLen_y[idx_V];
+                slope_to_add = V.slopes[idx_V];
+                ++idx_V;
+            } else { //insert next slope from v
+                len_x = v.SegLen_x[idx_v];
+                len_y = v.SegLen_y[idx_v];
+                slope_to_add = v.slopes[idx_v];
+                ++idx_v;
+            }
+            x += len_x; //next x value
+            y += len_y; //next y value
+            if (IC_curves[i].slopes.back() != slope_to_add) { //add a new segment
+                IC_curves[i].x.push_back(x);
+                IC_curves[i].y.push_back(y);
+                IC_curves[i].SegLen_x.push_back(len_x);
+                IC_curves[i].SegLen_y.push_back(len_y);
+                IC_curves[i].slopes.push_back(slope_to_add);
+            } else { //extend the previous segment
+                IC_curves[i].x.back() = x;
+                IC_curves[i].y.back() = y;
+                IC_curves[i].SegLen_x.back() += len_x;
+                IC_curves[i].SegLen_y.back() += len_y;
             }
         }
-    }
-    int v_idx = 0;
-    int V_idx = 0;
-    while (V_idx < V.NbEnds || v_idx < v.NbEnds){
-        if (V_idx >= V.NbEnds - 1 || v.slopes[v_idx] >= V.slopes[V_idx]){
-            
-        }
+        IC_curves[i].NbEnds = IC_curves[i].x.size();
+        IC_curves[i].NbSegs = IC_curves[i].NbEnds - 1;
+        IC_curves[i].NbConsegs = 1;
+        IC_curves[i].con_index.push_back(0);
+        IC_curves[i].con_index.push_back(IC_curves[i].NbSegs);
     }
 };
 
 void join(Curve& V, const Curve& raised_V, const std::vector<Curve>& IC_curves){
-
+    
 };
 
 void v2V(Curve& V, const Curve& v){
